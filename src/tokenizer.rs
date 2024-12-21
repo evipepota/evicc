0b1bf4f421b4f3c57098fc0887bd5338a11947b6
evipepota <shrimpround12@gmail.com>
2024-12-21 17:19:09 +0900

    feat: improved error messages

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,4 +1,11 @@
 use std::process;
+use std::sync::RwLock;
+
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref USER_INPUT: RwLock<Option<String>> = RwLock::new(None);
+}
 
 pub enum TokenKind {
     TkReserved,
@@ -11,21 +18,34 @@ pub struct Token {
     next: Option<Box<Token>>,
     val: Option<i32>,
     str: String,
+    loc: usize, // token location in input
 }
 
 impl Token {
-    fn new(kind: TokenKind, val: Option<i32>, str: String) -> Self {
+    fn new(kind: TokenKind, val: Option<i32>, str: String, loc: usize) -> Self {
         Token {
             kind,
             next: None,
             val,
             str,
+            loc,
         }
     }
 }
 
 fn error(msg: &str) -> ! {
-    eprintln!("{}", msg);
+    eprintln!("Error: {}", msg);
+    process::exit(1);
+}
+
+fn error_at(loc: usize, msg: &str) -> ! {
+    if let Some(ref user_input) = *USER_INPUT.write().unwrap() {
+        let input = &user_input;
+        eprintln!("{}", input);
+        eprintln!("{:>width$}^ {}", "", msg, width = loc);
+    } else {
+        eprintln!("Error: {}", msg);
+    }
     process::exit(1);
 }
 
@@ -43,7 +63,9 @@ pub fn consume(op: char, token: &mut Option<Box<Token>>) -> bool {
 
 pub fn expect(op: char, token: &mut Option<Box<Token>>) {
     if !consume(op, token) {
-        error(&format!("expected token is '{}'", op));
+        if let Some(current) = token {
+            error_at(current.loc, &format!("expected token is '{}'", op));
+        }
     }
 }
 
@@ -54,8 +76,10 @@ pub fn expect_number(token: &mut Option<Box<Token>>) -> i32 {
             *token = current.next.take();
             return val;
         }
+        error_at(current.loc, "expected number");
+    } else {
+        error("unexpected error");
     }
-    error("expected number");
 }
 
 pub fn at_eof(token: &Option<Box<Token>>) -> bool {
@@ -66,16 +90,18 @@ pub fn at_eof(token: &Option<Box<Token>>) -> bool {
     }
 }
 
-fn new_token(kind: TokenKind, cur: &mut Token, str: String) -> &mut Token {
-    let tok = Token::new(kind, None, str);
+fn new_token(kind: TokenKind, cur: &mut Token, str: String, loc: usize) -> &mut Token {
+    let tok = Token::new(kind, None, str.to_string(), loc);
     cur.next = Some(Box::new(tok));
     cur.next.as_mut().unwrap()
 }
 
 pub fn tokenizer(input: &str) -> Option<Box<Token>> {
-    let mut head = Token::new(TokenKind::TkEof, None, String::new());
+    let mut head = Token::new(TokenKind::TkEof, None, String::new(), 0);
     let mut cur = &mut head;
     let mut chars = input.chars().peekable();
+
+    *USER_INPUT.write().unwrap() = Some(input.to_string());
 
     while let Some(&c) = chars.peek() {
         if c.is_whitespace() {
@@ -84,7 +110,12 @@ pub fn tokenizer(input: &str) -> Option<Box<Token>> {
         }
 
         if c == '+' || c == '-' {
-            cur = new_token(TokenKind::TkReserved, cur, c.to_string());
+            cur = new_token(
+                TokenKind::TkReserved,
+                cur,
+                c.to_string(),
+                input.len() - chars.clone().count(),
+            );
             chars.next();
             continue;
         }
@@ -99,14 +130,23 @@ pub fn tokenizer(input: &str) -> Option<Box<Token>> {
                     break;
                 }
             }
-            cur = new_token(TokenKind::TkNum, cur, num_str.clone());
+            cur = new_token(
+                TokenKind::TkNum,
+                cur,
+                num_str.to_string(),
+                input.len() - chars.clone().count(),
+            );
             cur.val = Some(num_str.parse().unwrap());
             continue;
         }
 
-        error("invalid token");
+        if cur.loc == 0 {
+            error_at(cur.loc, "invalid token");
+        } else {
+            error_at(cur.loc + 1, "invalid token");
+        }
     }
 
-    new_token(TokenKind::TkEof, cur, String::new());
+    new_token(TokenKind::TkEof, cur, String::new(), input.len());
     head.next
 }
